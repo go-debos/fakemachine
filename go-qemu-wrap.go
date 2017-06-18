c2a664611d773aa73781daa54694bdd682f704e4
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-06-18 03:02:51 +0200

    Run go fmt

--- go-qemu-wrap.go
+++ go-qemu-wrap.go
@@ -1,53 +1,51 @@
 package main
 
 import (
-  "flag"
-  "os"
-  "os/exec"
-  "io"
-  "log"
-  "fmt"
-  "syscall"
-  "path"
-  "strings"
-  "path/filepath"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+	"syscall"
 
-  "github.com/surma/gocpio"
+	"github.com/surma/gocpio"
 )
 
 var count int
 
 type WriterHelper struct {
-  paths map[string]bool
-  *cpio.Writer
+	paths map[string]bool
+	*cpio.Writer
 }
 
-func NewWriterHelper (f io.Writer) *WriterHelper {
-  return &WriterHelper{
-    paths: map[string]bool{ "/": true },
-    Writer: cpio.NewWriter(f),
-  }
+func NewWriterHelper(f io.Writer) *WriterHelper {
+	return &WriterHelper{
+		paths:  map[string]bool{"/": true},
+		Writer: cpio.NewWriter(f),
+	}
 }
 
 func charsToString(in []int8) string {
-  s := make([]byte, len(in))
+	s := make([]byte, len(in))
 
-  i := 0
-  for ; i < len(in) ; i++ {
-    if in[i] == 0 {
-      break
-    }
-    s[i] = byte(in[i])
-  }
+	i := 0
+	for ; i < len(in); i++ {
+		if in[i] == 0 {
+			break
+		}
+		s[i] = byte(in[i])
+	}
 
-  return string(s[0:i])
+	return string(s[0:i])
 }
 
 const InitrdPath = "/tmp/initramfs.go.cpio"
 
-
-const InitScript =
-`#!/usr/bin/busybox sh
+const InitScript = `#!/usr/bin/busybox sh
 
 busybox mount -t proc proc /proc
 busybox mount -t sysfs none /sys
@@ -106,273 +104,271 @@ SendSIGHUP=yes
 `
 
 func EnsureBaseDirectory(w *WriterHelper, directory string) {
-  d := path.Clean(directory)
+	d := path.Clean(directory)
 
-  if w.paths[d] {
-    return
-  }
+	if w.paths[d] {
+		return
+	}
 
-  components := strings.Split(directory, "/")
-  collector := "/"
+	components := strings.Split(directory, "/")
+	collector := "/"
 
-  for _, c := range(components) {
-    collector = path.Join(collector, c)
-    if w.paths[collector] {
-      continue
-    }
+	for _, c := range components {
+		collector = path.Join(collector, c)
+		if w.paths[collector] {
+			continue
+		}
 
-    WriteDirectory(w, collector, 0755)
-  }
+		WriteDirectory(w, collector, 0755)
+	}
 }
 
 func WriteDirectory(w *WriterHelper, directory string, perm os.FileMode) {
-  EnsureBaseDirectory(w, path.Dir(directory))
+	EnsureBaseDirectory(w, path.Dir(directory))
 
-  hdr := new(cpio.Header)
+	hdr := new(cpio.Header)
 
-  hdr.Type = cpio.TYPE_DIR
-  hdr.Name = directory
-  hdr.Mode = int64(perm)
+	hdr.Type = cpio.TYPE_DIR
+	hdr.Name = directory
+	hdr.Mode = int64(perm)
 
-  w.WriteHeader(hdr)
+	w.WriteHeader(hdr)
 
-  w.paths[directory] = true
+	w.paths[directory] = true
 }
 
 func WriteFile(w *WriterHelper, file, content string, perm os.FileMode) {
-  EnsureBaseDirectory(w, path.Dir(file))
+	EnsureBaseDirectory(w, path.Dir(file))
 
-  hdr := new(cpio.Header)
+	hdr := new(cpio.Header)
 
-  bytes := []byte(content)
+	bytes := []byte(content)
 
-  hdr.Type = cpio.TYPE_REG
-  hdr.Name = file
-  hdr.Mode = int64(perm)
-  hdr.Size = int64(len(bytes))
+	hdr.Type = cpio.TYPE_REG
+	hdr.Name = file
+	hdr.Mode = int64(perm)
+	hdr.Size = int64(len(bytes))
 
-  w.WriteHeader(hdr)
-  w.Write(bytes)
+	w.WriteHeader(hdr)
+	w.Write(bytes)
 }
 
 func WriteSymlink(w *WriterHelper, target, link string, perm os.FileMode) {
-  EnsureBaseDirectory(w, path.Dir(link))
-  hdr := new(cpio.Header)
+	EnsureBaseDirectory(w, path.Dir(link))
+	hdr := new(cpio.Header)
 
-  content := []byte(target)
+	content := []byte(target)
 
-  hdr.Type = cpio.TYPE_SYMLINK
-  hdr.Name = link
-  hdr.Mode = int64(perm)
-  hdr.Size = int64(len(content))
+	hdr.Type = cpio.TYPE_SYMLINK
+	hdr.Name = link
+	hdr.Mode = int64(perm)
+	hdr.Size = int64(len(content))
 
-  w.WriteHeader(hdr)
-  w.Write(content)
+	w.WriteHeader(hdr)
+	w.Write(content)
 }
 
 func WriteCharDevice(w *WriterHelper, device string, major, minor int64,
-                 perm os.FileMode) {
-  EnsureBaseDirectory(w, path.Dir(device))
-  hdr := new(cpio.Header)
+	perm os.FileMode) {
+	EnsureBaseDirectory(w, path.Dir(device))
+	hdr := new(cpio.Header)
 
-  hdr.Type = cpio.TYPE_CHAR
-  hdr.Name = device
-  hdr.Mode = int64(perm)
-  hdr.Devmajor = major
-  hdr.Devminor = minor
+	hdr.Type = cpio.TYPE_CHAR
+	hdr.Name = device
+	hdr.Mode = int64(perm)
+	hdr.Devmajor = major
+	hdr.Devminor = minor
 
-  w.WriteHeader(hdr)
+	w.WriteHeader(hdr)
 }
 
-func CopyTree (w *WriterHelper, path string) {
-  walker := func(p string, info os.FileInfo, err error) error {
-    if info.Mode().IsDir() {
-      WriteDirectory(w, p, info.Mode() & ^os.ModeType)
-    } else if info.Mode().IsRegular() {
-      CopyFile(w, p)
-    } else {
-      panic("No handled")
-    }
+func CopyTree(w *WriterHelper, path string) {
+	walker := func(p string, info os.FileInfo, err error) error {
+		if info.Mode().IsDir() {
+			WriteDirectory(w, p, info.Mode() & ^os.ModeType)
+		} else if info.Mode().IsRegular() {
+			CopyFile(w, p)
+		} else {
+			panic("No handled")
+		}
 
-    return nil;
-  }
+		return nil
+	}
 
-  filepath.Walk(path, walker)
+	filepath.Walk(path, walker)
 }
 
 func CopyFile(w *WriterHelper, in string) error {
-  EnsureBaseDirectory(w, path.Dir(in))
+	EnsureBaseDirectory(w, path.Dir(in))
 
-  f, err := os.Open(in)
-  if (err != nil) {
-    log.Panicf("open failed: %s - %v", in, err)
-    return err
-  }
-  defer f.Close()
+	f, err := os.Open(in)
+	if err != nil {
+		log.Panicf("open failed: %s - %v", in, err)
+		return err
+	}
+	defer f.Close()
 
-  info, err := f.Stat()
-  if (err != nil) {
-    return err
-  }
+	info, err := f.Stat()
+	if err != nil {
+		return err
+	}
 
-  hdr := new(cpio.Header)
+	hdr := new(cpio.Header)
 
-  hdr.Type = cpio.TYPE_REG
-  hdr.Name = in
-  hdr.Mode = int64(info.Mode() & ^os.ModeType)
-  hdr.Size = info.Size()
+	hdr.Type = cpio.TYPE_REG
+	hdr.Name = in
+	hdr.Mode = int64(info.Mode() & ^os.ModeType)
+	hdr.Size = info.Size()
 
-  w.WriteHeader(hdr)
-  io.Copy(w, f)
+	w.WriteHeader(hdr)
+	io.Copy(w, f)
 
-  return nil
+	return nil
 }
 
-func AppendStaticVirtFS(qemuargs []string, directory, label string) ([]string) {
-    return append(qemuargs, "-virtfs",
-        fmt.Sprintf("local,mount_tag=%s,path=%s,security_model=none", label, directory))
+func AppendStaticVirtFS(qemuargs []string, directory, label string) []string {
+	return append(qemuargs, "-virtfs",
+		fmt.Sprintf("local,mount_tag=%s,path=%s,security_model=none", label, directory))
 }
 
 func AppendVirtFS(qemuargs, kernelargs []string, directory string) ([]string, []string) {
 
-    label := fmt.Sprintf("virtfs-%d", count)
-    count = count + 1
-    cmd := exec.Command("systemd-escape", "-p", directory)
-    output, _ := cmd.Output()
-    kernelargs = append(kernelargs,
-                        fmt.Sprintf("wrap.mount=%s:%s", label, output))
-    return AppendStaticVirtFS(qemuargs, directory, label), kernelargs
+	label := fmt.Sprintf("virtfs-%d", count)
+	count = count + 1
+	cmd := exec.Command("systemd-escape", "-p", directory)
+	output, _ := cmd.Output()
+	kernelargs = append(kernelargs,
+		fmt.Sprintf("wrap.mount=%s:%s", label, output))
+	return AppendStaticVirtFS(qemuargs, directory, label), kernelargs
 }
 
-
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  f, err := os.OpenFile(InitrdPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+	f, err := os.OpenFile(InitrdPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  w := NewWriterHelper(f)
+	w := NewWriterHelper(f)
 
-  WriteDirectory(w, "/scratch", 01777)
-  WriteDirectory(w, "/tmp", 01777)
-  WriteDirectory(w, "/var", 0755)
-  WriteDirectory(w, "/var/tmp", 01777)
-  WriteDirectory(w, "/var/lib", 0755)
-  WriteDirectory(w, "/var/lib/dbus", 0755)
-  WriteDirectory(w, "/sys", 0755)
-  WriteDirectory(w, "/proc", 0755)
-  WriteDirectory(w, "/run", 0755)
-  WriteDirectory(w, "/usr", 0755)
-  WriteDirectory(w, "/usr/bin", 0755)
-  WriteDirectory(w, "/lib64", 0755)
+	WriteDirectory(w, "/scratch", 01777)
+	WriteDirectory(w, "/tmp", 01777)
+	WriteDirectory(w, "/var", 0755)
+	WriteDirectory(w, "/var/tmp", 01777)
+	WriteDirectory(w, "/var/lib", 0755)
+	WriteDirectory(w, "/var/lib/dbus", 0755)
+	WriteDirectory(w, "/sys", 0755)
+	WriteDirectory(w, "/proc", 0755)
+	WriteDirectory(w, "/run", 0755)
+	WriteDirectory(w, "/usr", 0755)
+	WriteDirectory(w, "/usr/bin", 0755)
+	WriteDirectory(w, "/lib64", 0755)
 
-  WriteSymlink(w, "/run", "/var/run", 0755)
-  WriteSymlink(w, "/usr/sbin", "/sbin", 0755)
-  WriteSymlink(w, "/usr/bin", "/bin", 0755)
-  WriteSymlink(w, "/usr/lib", "/lib", 0755)
+	WriteSymlink(w, "/run", "/var/run", 0755)
+	WriteSymlink(w, "/usr/sbin", "/sbin", 0755)
+	WriteSymlink(w, "/usr/bin", "/bin", 0755)
+	WriteSymlink(w, "/usr/lib", "/lib", 0755)
 
-  CopyFile(w, "/lib64/ld-linux-x86-64.so.2")
-  CopyFile(w, "/usr/lib/x86_64-linux-gnu/libc.so.6")
-  // TODO broken with non-merged usr 
-  CopyFile(w, "/usr/bin/busybox")
+	CopyFile(w, "/lib64/ld-linux-x86-64.so.2")
+	CopyFile(w, "/usr/lib/x86_64-linux-gnu/libc.so.6")
+	// TODO broken with non-merged usr
+	CopyFile(w, "/usr/bin/busybox")
 
-  WriteCharDevice(w, "/dev/console", 5, 1, 0700)
+	WriteCharDevice(w, "/dev/console", 5, 1, 0700)
 
-  // Linker configuration
-  CopyFile(w, "/etc/ld.so.conf")
-  CopyTree(w, "/etc/ld.so.conf.d")
+	// Linker configuration
+	CopyFile(w, "/etc/ld.so.conf")
+	CopyTree(w, "/etc/ld.so.conf.d")
 
-  // Core system configuration
-  WriteFile(w, "/etc/machine-id", "", 0444)
-  WriteFile(w, "/etc/hostname", "wrapped-box", 0444)
+	// Core system configuration
+	WriteFile(w, "/etc/machine-id", "", 0444)
+	WriteFile(w, "/etc/hostname", "wrapped-box", 0444)
 
-  CopyFile(w, "/etc/passwd")
-  CopyFile(w, "/etc/group")
-  CopyFile(w, "/etc/nsswitch.conf")
+	CopyFile(w, "/etc/passwd")
+	CopyFile(w, "/etc/group")
+	CopyFile(w, "/etc/nsswitch.conf")
 
-  WriteFile(w, "/etc/systemd/network/ethernet.network", Networkd, 0444)
-  WriteSymlink(w,
-    "/lib/systemd/resolv.conf",
-    "/etc/resolv.conf",
-    0755)
-  WriteSymlink(w,
-    "/us//lib/systemd/system/systemd-networkd.service",
-    "/etc/systemd/system/multi-user.target.wants/systemd-networkd.service",
-    0755)
+	WriteFile(w, "/etc/systemd/network/ethernet.network", Networkd, 0444)
+	WriteSymlink(w,
+		"/lib/systemd/resolv.conf",
+		"/etc/resolv.conf",
+		0755)
+	WriteSymlink(w,
+		"/us//lib/systemd/system/systemd-networkd.service",
+		"/etc/systemd/system/multi-user.target.wants/systemd-networkd.service",
+		0755)
 
-  WriteSymlink(w,
-    "/etc/systemd/system/multi-user.target.wants/systemd-resolved.service",
-    "/us//lib/systemd/system/systemd-resolved.service",
-    0755)
+	WriteSymlink(w,
+		"/etc/systemd/system/multi-user.target.wants/systemd-resolved.service",
+		"/us//lib/systemd/system/systemd-resolved.service",
+		0755)
 
-  WriteSymlink(w,
-    "/etc/systemd/system/sockets.target.wants/systemd-networkd.socket",
-    "/usr/lib/systemd/system/systemd-networkd.socket",
-    0755)
+	WriteSymlink(w,
+		"/etc/systemd/system/sockets.target.wants/systemd-networkd.socket",
+		"/usr/lib/systemd/system/systemd-networkd.socket",
+		0755)
 
-  // TODO kernel modues
-  var u syscall.Utsname
-  syscall.Uname(&u)
-  kernelRelease :=  charsToString(u.Release[:])
+	// TODO kernel modues
+	var u syscall.Utsname
+	syscall.Uname(&u)
+	kernelRelease := charsToString(u.Release[:])
 
-  modules := []string{
-            "kernel/drivers/virtio/virtio.ko",
-            "kernel/drivers/virtio/virtio_pci.ko",
-            "kernel/net/9p/9pnet.ko",
-            "kernel/drivers/virtio/virtio_ring.ko",
-            "kernel/fs/9p/9p.ko",
-            "kernel/net/9p/9pnet_virtio.ko",
-            "kernel/fs/fscache/fscache.ko",
-            "modules.order",
-            "modules.builtin",
-            "modules.dep",
-            "modules.dep.bin",
-            "modules.alias",
-            "modules.alias.bin",
-            "modules.softdep",
-            "modules.symbols",
-            "modules.symbols.bin",
-            "modules.builtin.bin",
-            "modules.devname" }
+	modules := []string{
+		"kernel/drivers/virtio/virtio.ko",
+		"kernel/drivers/virtio/virtio_pci.ko",
+		"kernel/net/9p/9pnet.ko",
+		"kernel/drivers/virtio/virtio_ring.ko",
+		"kernel/fs/9p/9p.ko",
+		"kernel/net/9p/9pnet_virtio.ko",
+		"kernel/fs/fscache/fscache.ko",
+		"modules.order",
+		"modules.builtin",
+		"modules.dep",
+		"modules.dep.bin",
+		"modules.alias",
+		"modules.alias.bin",
+		"modules.softdep",
+		"modules.symbols",
+		"modules.symbols.bin",
+		"modules.builtin.bin",
+		"modules.devname"}
 
-  for _,v := range modules {
-    CopyFile(w, path.Join("/usr/lib/modules", kernelRelease, v))
-  }
+	for _, v := range modules {
+		CopyFile(w, path.Join("/usr/lib/modules", kernelRelease, v))
+	}
 
-  WriteFile(w, "etc/systemd/system/serial-getty@ttyS0.service",
-    fmt.Sprintf(ServiceTemplate, "/bin/bash"), 0755)
+	WriteFile(w, "etc/systemd/system/serial-getty@ttyS0.service",
+		fmt.Sprintf(ServiceTemplate, "/bin/bash"), 0755)
 
-  WriteFile(w, "/init", InitScript, 0755)
+	WriteFile(w, "/init", InitScript, 0755)
 
-  w.Close()
-  f.Close()
+	w.Close()
+	f.Close()
 
-  qemuargs := []string{ "qemu-system-x86_64",
-                        "-cpu", "host",
-                        "-smp", "2",
-                        "-m", "2048",
-                        "-enable-kvm",
-                        "-kernel", "/boot/vmlinuz-" + kernelRelease,
-                        "-initrd", InitrdPath,
-                        "-nographic",
-                        "-no-reboot" }
-  kernelargs := []string{"console=ttyS0", "quiet", "panic=-1"}
+	qemuargs := []string{"qemu-system-x86_64",
+		"-cpu", "host",
+		"-smp", "2",
+		"-m", "2048",
+		"-enable-kvm",
+		"-kernel", "/boot/vmlinuz-" + kernelRelease,
+		"-initrd", InitrdPath,
+		"-nographic",
+		"-no-reboot"}
+	kernelargs := []string{"console=ttyS0", "quiet", "panic=-1"}
 
-  qemuargs = AppendStaticVirtFS(qemuargs, "/usr", "usr")
+	qemuargs = AppendStaticVirtFS(qemuargs, "/usr", "usr")
 
-  qemuargs, kernelargs = AppendVirtFS(qemuargs, kernelargs, "/etc/ssl")
-  qemuargs, kernelargs = AppendVirtFS(qemuargs, kernelargs, "/etc/alternatives")
+	qemuargs, kernelargs = AppendVirtFS(qemuargs, kernelargs, "/etc/ssl")
+	qemuargs, kernelargs = AppendVirtFS(qemuargs, kernelargs, "/etc/alternatives")
 
-  qemuargs = append(qemuargs, "-append", strings.Join(kernelargs, " "))
+	qemuargs = append(qemuargs, "-append", strings.Join(kernelargs, " "))
 
-  pa := os.ProcAttr {
-    Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-  }
+	pa := os.ProcAttr{
+		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+	}
 
-
-  p, _ := os.StartProcess("/usr/bin/qemu-system-x86_64", qemuargs, &pa)
-  p.Wait()
+	p, _ := os.StartProcess("/usr/bin/qemu-system-x86_64", qemuargs, &pa)
+	p.Wait()
 }
