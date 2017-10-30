226dd8fed355d1a104d2c506e1e67f38250cd814
Sjoerd Simons <sjoerd.simons@collabora.co.uk>
2017-10-30 14:01:56 +0100

    Add image label infrastructure
    Label images passed or created by fakemachine such that they can be
    correlated inside the fake machine. The user can optionally specify
    their own label as long as it's inside the constraints of the qemu
    serial id and doesn't clash.
    
    Signed-off-by: Sjoerd Simons <sjoerd.simons@collabora.co.uk>

--- cmd/fakemachine/main.go
+++ cmd/fakemachine/main.go
@@ -38,17 +38,18 @@ func SetupImages(m *fakemachine.Machine, options Options) {
 	for _, i := range options.Images {
 		parts := strings.Split(i, ":")
 		var err error
+		var l string
 
 		switch len(parts) {
 		case 1:
-			err = m.CreateImage(parts[0], -1)
+			l, err = m.CreateImage(parts[0], -1)
 		case 2:
 			var size int64
 			size, err = units.FromHumanSize(parts[1])
 			if err != nil {
 				break
 			}
-			err = m.CreateImage(parts[0], size)
+			l, err = m.CreateImage(parts[0], size)
 		default:
 			fmt.Fprintf(os.Stderr, "Failed to parse image: %s\n", i)
 			os.Exit(1)
@@ -58,6 +59,8 @@ func SetupImages(m *fakemachine.Machine, options Options) {
 			fmt.Fprintf(os.Stderr, "Failed to create image: %s %v\n", i, err)
 			os.Exit(1)
 		}
+
+		fmt.Printf("Exposing %s as %s\n", parts[0], l)
 	}
 }
 
