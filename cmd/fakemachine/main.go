9ae422452602fa0de1ec2b4b52cc61dbd8f1cded
Christopher Obbard <chris.obbard@collabora.com>
2020-11-22 10:32:42 +0000

    backend: allow choice of backend
    The Machine object allows the backend name to be set using the
    NewMachineWithBackend method. The backend is used when starting the
    machine and allows the implementation detail of the particular
    virtualisation technology to be hidden from the user.
    
    Also for the fakemachine binary accept a cmdline parameter --backend
    which allows the user to set a specific backend when calling
    fakemachine manually.
    
    By default the backend name is set to "auto" which should choose the best
    supported virtualisation technology on this machine, making sure we log
    the backend used. No breaking API changes are introduced.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- cmd/fakemachine/main.go
+++ cmd/fakemachine/main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type Options struct {
+	Backend	    string   `short:"b" long:"backend" description:"Virtualisation backend to use" default:"auto"`
 	Volumes     []string `short:"v" long:"volume" description:"volume to mount"`
 	Images      []string `short:"i" long:"image" description:"image to add"`
 	Memory      int      `short:"m" long:"memory" description:"Amount of memory for the fakemachine in megabytes"`
@@ -68,6 +69,10 @@ func SetupImages(m *fakemachine.Machine, options Options) {
 }
 
 func main() {
+	// append the list of available backends to the commandline argument parser
+	opt := parser.FindOptionByLongName("backend")
+	opt.Choices = fakemachine.BackendNames()
+
 	args, err := parser.Parse()
 	if err != nil {
 		flagsErr, ok := err.(*flags.Error)
@@ -78,7 +83,12 @@ func main() {
 		}
 	}
 
-	m := fakemachine.NewMachine()
+	m, err := fakemachine.NewMachineWithBackend(options.Backend)
+	if err != nil {
+		fmt.Printf("fakemachine: %v\n", err)
+		os.Exit(1)
+	}
+
 	m.SetShowBoot(options.ShowBoot)
 	SetupVolumes(m, options)
 	SetupImages(m, options)
