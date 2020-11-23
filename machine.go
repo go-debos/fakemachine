6fec753083df48ee8ee87684285c2106b81f6f21
Christopher Obbard <chris.obbard@collabora.com>
2020-11-23 08:45:35 +0000

    backend: implement start function
    Move all backend-related functionality from Machine startup function
    into the backend implementation.
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- machine.go
+++ machine.go
@@ -487,6 +487,11 @@ func (m *Machine) startup(command string, extracontent [][2]string) (int, error)
 
 	backend := m.backend
 
+	_, kernelModuleDir, err := hostKernelPath()
+	if err != nil {
+		return -1, err
+	}
+
 	w := writerhelper.NewWriterHelper(f)
 
 	w.WriteDirectory("/scratch", 01777)
@@ -597,72 +602,9 @@ func (m *Machine) startup(command string, extracontent [][2]string) (int, error)
 	w.Close()
 	f.Close()
 
-	kernelRelease, err := m.kernelRelease()
-	if err != nil {
-		return -1, err
-	}
-	memory := fmt.Sprintf("%d", m.memory)
-	numcpus := fmt.Sprintf("%d", m.numcpus)
-	qemuargs := []string{"qemu-system-x86_64",
-		"-cpu", "host",
-		"-smp", numcpus,
-		"-m", memory,
-		"-enable-kvm",
-		"-kernel", "/boot/vmlinuz-" + kernelRelease,
-		"-initrd", m.initrdpath,
-		"-display", "none",
-		"-no-reboot"}
-	kernelargs := []string{"console=ttyS0", "panic=-1",
-		"systemd.unit=fakemachine.service"}
-
-	if m.showBoot {
-		// Create a character device representing our stdio
-		// file descriptors, and connect the emulated serial
-		// port (which is the console device for the BIOS,
-		// Linux and systemd, and is also connected to the
-		// fakemachine script) to that device
-		qemuargs = append(qemuargs,
-			"-chardev", "stdio,id=for-ttyS0,signal=off",
-			"-serial", "chardev:for-ttyS0")
-	} else {
-		qemuargs = append(qemuargs,
-			// Create the bus for virtio consoles
-			"-device", "virtio-serial",
-			// Create /dev/ttyS0 to be the VM console, but
-			// ignore anything written to it, so that it
-			// doesn't corrupt our terminal
-			"-chardev", "null,id=for-ttyS0",
-			"-serial", "chardev:for-ttyS0",
-			// Connect the fakemachine script to our stdio
-			// file descriptors
-			"-chardev", "stdio,id=for-hvc0,signal=off",
-			"-device", "virtconsole,chardev=for-hvc0")
-	}
-
-	for _, point := range m.mounts {
-		qemuargs = append(qemuargs, "-virtfs",
-			fmt.Sprintf("local,mount_tag=%s,path=%s,security_model=none",
-				point.label, point.hostDirectory))
-	}
-
-	for i, img := range m.images {
-		qemuargs = append(qemuargs, "-drive",
-			fmt.Sprintf("file=%s,if=none,format=raw,cache=unsafe,id=drive-virtio-disk%d", img.path, i))
-		qemuargs = append(qemuargs, "-device",
-			fmt.Sprintf("virtio-blk-pci,drive=drive-virtio-disk%d,id=virtio-disk%d,serial=%s",
-				i, i, img.label))
-	}
-
-	qemuargs = append(qemuargs, "-append", strings.Join(kernelargs, " "))
-
-	pa := os.ProcAttr{
-		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
-	}
-
-	if p, err := os.StartProcess("/usr/bin/qemu-system-x86_64", qemuargs, &pa); err != nil {
-		return -1, err
-	} else {
-		p.Wait()
+	success, err := backend.Start()
+	if !success || err != nil {
+		return -1, fmt.Errorf("error starting %s backend: %v", backend.Name(), err)
 	}
 
 	result, err := os.Open(path.Join(tmpdir, "result"))
