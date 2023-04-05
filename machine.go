5bc56c34e621a1e738e12c3117f83b075187aa0a
Andrej Shadura <andrew.shadura@collabora.co.uk>
2023-04-05 11:16:10 +0200

    Wrap errors where possible and makes sense
    Using %w instead of %v makes Errorf store the original error, allowing
    the caller to extract and use it later. While there isn’t any immediate
    benefit from it, it may be useful to the users of fakemachine.
    
    See:
     * https://go.dev/doc/go1.13#error_wrapping
     * https://github.com/golang/go/wiki/ErrorValueFAQ
     * https://go.googlesource.com/proposal/+/master/design/29934-error-values.md
    
    Signed-off-by: Andrej Shadura <andrew.shadura@collabora.co.uk>

--- machine.go
+++ machine.go
@@ -843,7 +843,7 @@ func (m *Machine) startup(command string, extracontent [][2]string) (int, error)
 
 	success, err := backend.Start()
 	if !success || err != nil {
-		return -1, fmt.Errorf("error starting %s backend: %v", backend.Name(), err)
+		return -1, fmt.Errorf("error starting %s backend: %w", backend.Name(), err)
 	}
 
 	result, err := os.Open(path.Join(tmpdir, "result"))
@@ -877,7 +877,7 @@ func (m *Machine) RunInMachineWithArgs(args []string) (int, error) {
 	executable, err := exec.LookPath(os.Args[0])
 
 	if err != nil {
-		return -1, fmt.Errorf("Failed to find executable: %v\n", err)
+		return -1, fmt.Errorf("Failed to find executable: %w\n", err)
 	}
 
 	return m.startup(command, [][2]string{{executable, name}})
